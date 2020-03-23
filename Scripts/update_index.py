edcda95947f9573297d3f3a00295022855087d1b
megatron <megatronPrima@gmail.com>
2020-03-23 16:45:16 +0800

    adjust

--- Scripts/update_index.py
+++ Scripts/update_index.py
@@ -5,19 +5,47 @@ import json
 import logging
 
 def collect_requirements(country):
-  logging.info("Collecting %s" % country)
+  #logging.info("Collecting %s" % country)
 
+  res = []
   for filename in os.listdir(country):
-    if filename.endswith(".geo.json"): 
-      logging.info(" - %s", filename)
+    if filename.endswith(".geo.json"):
+      #logging.info(" - %s", filename)
+      with open(os.path.join(country, filename), 'r') as load_file:
+        load_dict = json.load(load_file)
+        if 'features' not in load_dict or len(load_dict) < 1:
+          return
+        res.append({country: load_dict['features']})
 
-  return []
+  return res
 
 def update_country_index(country, requirements):
-  return {}
+  res = {}
+  country_request = {
+    'applicants': 0,
+    'beneficiaries': 0,
+    'masks_requested': 0,
+    'masks_delivered': 0
+  }
+
+  for request in requirements:
+    if country in request.keys():
+      for item in request[country]:
+        country_request['applicants'] += 1
+        country_request['beneficiaries'] += item['properties']['需要援助人数']
+        country_request['masks_requested'] += item['properties']['需求数量']
+        country_request['masks_delivered'] += item['properties']['收到数量']
+  res[country] = country_request
+  return res
 
 def append_world_index(requirements):
-  pass
+  if not requirements or requirements == {}:
+    return
+  for k, v in requirements.items():
+    world_summary['applicants'] += v['applicants']
+    world_summary['beneficiaries'] += v['beneficiaries']
+    world_summary['masks_requested'] += v['masks_requested']
+    world_summary['masks_delivered'] += v['masks_delivered']
 
 if __name__ == '__main__':
   logging.basicConfig(level=logging.INFO)
@@ -35,6 +63,10 @@ if __name__ == '__main__':
 
   for country in countries:
     country_requirements = collect_requirements(country)
+    logging.info(country_requirements)
     country_summary = update_country_index(country, country_requirements)
+    logging.info(country_summary)
+    append_world_index((country_summary))
+    logging.info(world_summary)
     world_requirments.extend(country_requirements)
-
+    logging.info(world_requirments)
