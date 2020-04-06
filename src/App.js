e3035ff4abf7b2717c7e0013afd92def31d9b634
yuniorzen <YuniorZen@qq.com>
2020-04-06 13:19:16 +0800

    Volunteer 组件

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import "./assets/styles/app.scss";
 // 引入页面组件
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Volunteer from './components/Volunteer'
 import Sponser from './components/Sponser'
 import HelpUs from './components/HelpUs'
 
@@ -62,6 +63,13 @@ const App = () => {
     setModalIsOpen(!modalIsOpen);
     setSelectedIndex(index);
   };
+
+  const updateVolunteerData=(data)=>{
+    if(data&&data.length>0){
+      setVolunteerCount(data.length)
+      setVolunteerData(data)
+    }
+  }
 
   const delta = 6;
   let startX;
@@ -147,15 +155,6 @@ const App = () => {
     };
     getForeigns(foreignData);
 
-    // 获取自愿者数据
-    request.getVolunteerData()
-    .then((response) => {
-      let { data } = response;
-      data = data.filter((d) => d["field_18"] === "已通过");
-      setVolunteerData(data);
-      setVolunteerCount(data.length);
-    })
-    .catch((error) => console.log(error));
 
     // 获取捐赠者数据
     const getDonators = (tmpData = [], next = "") => {
@@ -266,20 +265,6 @@ const App = () => {
     return a > b ? -1 : a < b ? 1 : 0;
   });
 
-  const volunteerList = volunteerData.map((data) => (
-    <div className="avatar">
-      <img
-        src={
-          data["x_field_weixin_headimgurl"].replace("http://", "https://") ||
-          images.avatar
-        }
-        alt="avatar"
-      />
-      <div className="overlay">
-        <div className="text">{data["field_1"]}</div>
-      </div>
-    </div>
-  ));
 
   const hightlightList = [];
   let imagesTemp = [];
@@ -509,36 +494,8 @@ const App = () => {
           ) : null}
         </ModalGateway>
 
-        <div className="volunteers">
-          <div className="column left">
-            <h2>默默付出着的志愿者们</h2>
-            <p>
-              坦诚的讲，做这样的公益太累了。如果没有这帮志愿者的陆续加入，我和@Char根本坚持不下来。
-            </p>
-            <p>要感谢的人太多，但我更想对这些同志们说——辛苦了！</p>
-            <p>Respect!</p>
-            <a
-              className="btn"
-              target="_blank"
-              rel="noopener noreferrer"
-              href={JINSHUJU.VOLUNTEER_FORM_LINK}
-              type="button"
-            >
-              加入我们
-            </a>
-          </div>
-          <div className="column right">{volunteerList}</div>
-          <a
-            className="btn-mb"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={JINSHUJU.VOLUNTEER_FORM_LINK}
-            type="button"
-          >
-            加入我们
-          </a>
-        </div>
-      
+        
+        <Volunteer updateVolunteerData={updateVolunteerData} />
       </section>
 
       <Sponser />
