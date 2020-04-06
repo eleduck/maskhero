d96d8ccac19b462802bb51b184cbf9928b173b88
yuniorzen <YuniorZen@qq.com>
2020-04-06 11:27:44 +0800

    Header组件

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import  moment from "moment";
 import "./assets/styles/common.scss";
 import "./assets/styles/app.scss";
 
+
+// 引入页面组件
+import Header from './components/Header'
+
 // 图片资源、金数据
 import {images, JINSHUJU} from "./utils/contants"
 
@@ -27,6 +31,8 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 am4core.useTheme(am4themes_animated);
+
+
 
 // TODO: workaround
 const TEMP_ENDPOINT = "/forms/Ukw1aQ/entries";
@@ -344,120 +350,9 @@ const App = () => {
         </div>
       </div>
 
-      <header className="header">
-        <img src={images.logo} className="logo" alt="logo" />
-        <img src={images.logomb} className="logo-mb" alt="logo" />
-        <img src={images.title} className="title" alt="title" />
-        <h1>海外的亲们，是时候让我们陪你们打下半场了！</h1>
-        <div className="bar-mb"></div>
-        <p>
-          这场疫情，国内打上半场，国外打下半场，海外华人打全场。”这话于国内的我们而言是段子，于国外的你们是猝不及防的遭遇。
-          在倾力支援国内之后，异国他乡的土地上的你们，除了对抗病毒本身，还要替我们承受“Chinese
-          Virus”的污名乃至这背后的威胁、暴力。
-        </p>
-        <p className="second">
-          这一切，国内的亲们，不会坐视。岂曰无衣，与子同袍！我们一起来打下半场。
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://eleduck.com/posts/6XNfOL"
-          >
-            点此查看我们正在做的>>>
-          </a>
-        </p>
-        <div className="wrapper">
-          <div className="tile">
-            <div className="name">援助口罩</div>
-            <div className="data">
-              {/* {maskCount} */}
-              {2000}
-              <span className="unit">个</span>
-            </div>
-          </div>
-          <div className="split"></div>
-          <div className="tile">
-            <div>收到捐款</div>
-            <div className="data">
-              <span>{money}</span>
-              {/* <span>{5180}</span> */}
-              <span className="unit">元</span>
-            </div>
-          </div>
-          <div className="split"></div>
-          <div className="tile">
-            <div>援助华人家庭</div>
-            <div className="data">
-              {/* {helpCount} */}
-              {200}
-              <span className="unit">个</span>
-            </div>
-          </div>
-          <div className="split"></div>
-          <div className="tile">
-            <div>志愿者</div>
-            <div className="data">
-              {volunteerCount}
-              {/* {18} */}
-              <span className="unit">人</span>
-            </div>
-          </div>
-          <div className="split"></div>
-          <div className="tile">
-            <div>援助海外城市</div>
-            <div className="data">
-              <span>4</span>
-              <span className="unit">个</span>
-            </div>
-          </div>
-        </div>
-        <img className="arrow" src={images.vector} alt="arrow"></img>
-      </header>
+      <Header money={money} volunteerCount={volunteerCount} />
 
-      <div className="wrapper-mb">
-        <h1>截止目前为止，我们已经</h1>
-        <div className="tile">
-          <div className="name">援助口罩</div>
-          <div className="data">
-            {2000}
-            {/* {maskCount} */}
-            <span className="unit">个</span>
-          </div>
-        </div>
-        <div className="split"></div>
-        <div className="tile">
-          <div className="name">收到捐款</div>
-          <div className="data">
-            <span>{money}</span>
-            {/* <span>{5180}</span> */}
-            <span className="unit">元</span>
-          </div>
-        </div>
-        <div className="split"></div>
-        <div className="tile">
-          <div className="name">援助华人家庭</div>
-          <div className="data">
-            {/* {helpCount} */}
-            {200}
-            <span className="unit">个</span>
-          </div>
-        </div>
-        <div className="split"></div>
-        <div className="tile">
-          <div className="name">志愿者</div>
-          <div className="data">
-            {volunteerCount}
-            <span className="unit">人</span>
-          </div>
-        </div>
-        <div className="split"></div>
-        <div className="tile">
-          <div className="name">援助海外城市</div>
-          <div className="data">
-            <span>4</span>
-            <span className="unit">个</span>
-          </div>
-        </div>
-      </div>
+      
 
       <section className="support">
         <h1>援助地图</h1>
